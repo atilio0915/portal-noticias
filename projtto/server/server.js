496273a3afeeddaa7b889da09a7492612c863a1d
atilio0915 <atilioalmeidabettii@gmail.com>
2025-02-18 13:01:07 -0300

    Adicionando server.js

--- projtto/server/server.js
+++ projtto/server/server.js
@@ -2,34 +2,59 @@ const express = require("express");
 const mysql = require("mysql2");
 const bcrypt = require("bcrypt");
 const session = require("express-session");
+const mysqlStore = require("express-mysql-session")(session);
+const cookieParser = require('cookie-parser');
+
+
 const app = express();
 let port = 3008;
 const cors = require("cors");
 
-app.use(cors());
+app.use(cookieParser()); 
+
+app.use(
+  cors({
+    origin: "http://localhost:3000", // Permitir apenas o frontend específico
+    methods: ["GET", "POST", "PUT", "DELETE"], // Métodos permitidos
+    credentials: true,
+  })
+);
 
 // Middleware para entender JSON no corpo da requisição
 app.use(express.json());
 
-// configurando o middleware de session 
+// Criar mysql da sessão
+const sessionStore = new mysqlStore({
+  host: "127.0.0.1",
+  user: "root",
+  password: "58",
+  database: "portal_db",
+  clearExpired: true,
+  checkExpirationInterval: 900000, // Verifica sessões expiradas a cada 15 min
+  expiration: 1000 * 60 * 30, // Expira em 30 min
+});
+
+// Configurando o middleware de session
 app.use(
   session({
-    secret:"chave",
-    resave:"true",
+    secret: "chave",
+    resave: false,
     saveUninitialized: true,
+    store: sessionStore,
     cookie: {
       maxAge: 1000 * 60 * 30, // 30 minutos
-      httpOnly: false,
+      httpOnly: true,
       secure: false,
-      sameSite: "strict",
+      sameSite: "lax",
     },
   })
-)
+);
+
 // Criando conexão com MySQL
 const connection = mysql.createConnection({
   host: "127.0.0.1",
   user: "root",
-  password: "58627094",
+  password: "58",
   database: "portal_db",
 });
 
@@ -45,33 +70,32 @@ connection.connect((err) => {
 // Rota para receber dados do formulário e cadastrar
 app.post("/formulario", (req, res) => {
   const { nome, senha } = req.body;
-  //verificar a senha e o nome com a expressao regular
   const nomeregular = /^[a-zA-Z0-9_-]{3,20}$/;
   if (!nomeregular.test(nome)) {
     return res
       .status(500)
-      .json({ mensagem: "nome minimo 3 caracteres, apenas numeros e letras" });
+      .json({ mensagem: "Nome mínimo 3 caracteres, apenas números e letras" });
   }
   if (!nomeregular.test(senha)) {
     return res
       .status(500)
-      .json({ mensagem: "senha minimo 3 caracteres, apenas numeros e letras" });
+      .json({ mensagem: "Senha mínima 3 caracteres, apenas números e letras" });
   }
 
-  // realizar consulta se o usuario ja existe
-  const query1 = "SELECT COUNT(nome) As count FROM cliente WHERE nome = ?";
+  // Realizar consulta se o usuário já existe
+  const query1 = "SELECT COUNT(nome) AS count FROM cliente WHERE nome = ?";
   connection.query(query1, [nome], (err, results) => {
     if (err) {
       console.error("Erro ao fazer a consulta:", err);
-      return res.status(500).json({ mensagem: "erro ao fazer a consulta" });
+      return res.status(500).json({ mensagem: "Erro ao fazer a consulta" });
     }
     if (results[0].count > 0) {
-      return res.status(400).json({ mensagem: "usuario ja existe" });
+      return res.status(400).json({ mensagem: "Usuário já existe" });
     } else {
-      //gerando hash da senha
+      // Gerando hash da senha
       bcrypt.hash(senha, 10, (err, hash) => {
         if (err) {
-          console.error("erro ao gerar hash");
+          console.error("Erro ao gerar hash");
           return;
         }
         // Inserindo dados no banco
@@ -82,6 +106,7 @@ app.post("/formulario", (req, res) => {
             res.status(500).json({
               status: "erro",
               mensagem: "Erro ao salvar os dados",
+              
             });
             return;
           }
@@ -97,60 +122,112 @@ app.post("/formulario", (req, res) => {
     }
   });
 });
-//rota para entrar com o usuario e senha 
-app.post("/entrar", (req,res)=>{
-    const {nome, senha} = req.body
-    const nomeregular = /^[a-zA-Z0-9_-]{3,20}$/;
-    if(!nomeregular.test(nome)){
-      return res.status(500).json({mensagem:'nome minimo 3 caracteres, apenas numeros e letras'})
+
+// Rota para entrar com o usuário e senha
+app.post("/entrar", (req, res) => {
+  const { nome, senha } = req.body;
+  const nomeregular = /^[a-zA-Z0-9_-]{3,20}$/;
+  if (!nomeregular.test(nome)) {
+    return res.status(500).json({ mensagem: "Nome mínimo 3 caracteres, apenas números e letras" });
+  }
+  if (!nomeregular.test(senha)) {
+    return res.status(500).json({ mensagem: "Nome mínimo 3 caracteres, apenas números e letras" });
+  }
+
+  // Realizar consulta se o usuário e senha são válidos
+  const query2 = "SELECT id, senha FROM cliente WHERE nome = ?";
+  connection.query(query2, [nome], (err, response) => {
+    if (err) {
+      console.log("Erro ao fazer a consulta de usuário");
+      return res.status(404).json({ mensagem: "Erro ao encontrar usuário" });
     }
-    if(!nomeregular.test(senha)){
-      return res.status(500).json({mensagem:'nome minimo 3 caracteres, apenas numeros e letras'})
+    if (response.length === 0) {
+      return res.status(404).json({ mensagem: "Usuário errado ou não encontrado" });
     }
-    // realizar consulta se o usuario e senha é valido
-    
-    const query2 = "select id,senha from cliente where nome = ?"
-    connection.query(query2,[nome],(err,response)=>{
-      if(err){
-        console.log('erro ao fazer a consulta de usuario')
-        return res.status(404).json({mensagem:'erro ao encontrar usuario'})
+
+    const user = response[0]; // Pegando o usuário
+    // Comparar senha do usuário com a senha do banco de dados que está em hash
+    bcrypt.compare(senha, user.senha, (err, result) => {
+      if (err) {
+        return res.status(500).json({ mensagem: "Erro ao verificar senha" });
       }
-      if(response.length === 0){
-        return res.status(404).json({mensagem: 'usuario errado ou não encontrado'})
+      if (result) {
+        // Gerar uma sessão para o cliente
+        req.session.user = { id: user.id, senha: user.senha };
+        console.log(req.sessionID)
+
+        res.cookie("user_id", req.session.user.id, {
+          maxAge: 1000 * 60 * 30, // 30 minutos
+          httpOnly: true,
+        });
+
+
+        // Salvar a sessão de forma assíncrona
+        req.session.save((err) => {
+          if (err) {
+            return res.status(500).json({ mensagem: "Erro ao salvar sessão" });
+          }
+          console.log("Sessão após salvar:", req.session); // Aqui já deve estar disponível
+
+          return res.status(200).json({
+            status: "sucesso",
+            mensagem: "Sessão criada",
+          });
+        });
+      } else {
+        return res.status(401).json({ mensagem: "Senha errada" });
       }
+    });
+  });
+});
 
-      const user = response[0] // pegando o usuario 
-      // comparar senha do usuario com a senha do banco de dados que esta em hash
-      bcrypt.compare(senha, user.senha,(err,response)=>{ // pegando a linha com user e a coluna senha user.senha
-        if(err){
-          return res.status(500).json({mensagem: 'erro ao verificar senha'})
-        }
-        if(response){
-          console.log("pode entrar")
-          // gerar uma sessao para o cliente 
-          req.session.user = user
-          return res.status(200).json({ 
-            status: 'sucesso',
-            mensagem: 'sessao criada'})
-          }else{
-          return res.status(401).json({mensagem: 'senha errada'})
-        }
-      }) 
-    })
-
-
-})
-// rota para logout
-app.post('/logout',(req,res) =>{
-  req.session.destroy((err)=>{
-    if(err){
-      return res.status(500).json({mensagem:'erro ao encerrar sessao'})
+// Rota para logout
+app.post("/logout", (req, res) => {
+  req.session.destroy((err) => {
+    if (err) {
+      return res.status(500).json({ mensagem: "Erro ao encerrar sessão" });
     }
     res.clearCookie("connect.sid");
-    res.status(200).json({mensagem:'logout realizado com sucesso'})
-  })
-})
+    res.status(200).json({ mensagem: "Logout realizado com sucesso" });
+  });
+});
 
+// Middleware para verificar autenticação
+function verificarAutenticacao(req, res, next) {
+  if ( !req.session.user || !req.session.user.id) {
+    console.log("Sessão inválida ou não autenticada:", req.session);
+    return res.status(401).json({ erro: "Usuário não autenticado" });
+  }
+  console.log("Usuário autenticado:", req.session.user.id);
+  next(); // Se o usuário estiver autenticado, segue para a próxima função
+}
+
+// Rota para pegar notícias salvas no banco
+app.get('/noticiasSalvas', (req, res) => {
+  const query = "SELECT * FROM noticiassalvas";
+  connection.query(query, (err, results) => {
+    if (err) return res.status(500).json({ erro: "Erro ao achar tabela de notícias salvas" });
+    res.json(results);
+  });
+});
+
+// Rota para adicionar notícias salvas
+app.post('/adicionarnoticias', verificarAutenticacao, (req, res) => {
+  console.log(req.body);
+  const noticia = req.body;
+  const titulo = noticia.title;
+  const url = noticia.url;
+  const img = noticia.urlToImage;
+  const descricao = noticia.description;
+  const userid =  req.cookies.user_id // Acessando diretamente a sessão para obter o ID do usuário
+  const query = 'INSERT INTO noticiassalvas (user_id, titulo, descricao, url, img) VALUES (?,?,?,?,?)';
+  connection.query(query, [userid, titulo, descricao, url, img], (err, results) => {
+    if (err) {
+      return res.status(500).json({ erro: "Erro ao inserir notícia" });
+    }
+    res.status(200).json({ mensagem: "Sucesso ao salvar notícia" });
+  });
+});
 
 // Iniciando o servidor
 app.listen(port, () => {
